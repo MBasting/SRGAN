efcac7feef1857b7720b210d5afc6eaef2b83339
jetidee <jesserolfes@gmail.com>
2022-03-13 22:09:49 +0100

    added the implementation of the authors.

--- DLoss.py
+++ DLoss.py
@@ -8,6 +8,21 @@ for training the images are labeled as real or fake: Yhr=1 Ysr=0.
 
 I do not know how to implement the DLoss function, in eq. 4 the BXEsr has a term that is always 0 namely Ysr. Therefor BXEsr=0
 The second problem is that both the BXEhr and BXEsr sum over N, yet there is no N behind the sum. I do not know what to sum over.
+
+implementation of the authors:
+
+with tf.variable_scope('Discriminator_loss'):
+                # the cross entropy should approach zero for perfect discrimination. random discrimination should be 0.5 for both, summing to 1           
+    #            d_loss = (d_loss_real + d_loss_fake)
+                
+                d_loss1 = tl.cost.sigmoid_cross_entropy(logits_real, tf.ones_like(logits_real), name='d1')
+                d_loss2 = tl.cost.sigmoid_cross_entropy(logits_fake, tf.zeros_like(logits_fake), name='d2')
+                
+                d_loss = d_loss1 + d_loss2
+            # here, fake is 0 if the discriminator is good.
+            # the generator aims to raise the label values towards 1
+
+
 """
 
 def DLoss():
