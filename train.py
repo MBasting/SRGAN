d817b6035a29b39a38b6c11e8766be5dae4da091
Mark Basting <m.j.basting@student.tudelft.nl>
2022-03-15 19:47:15 +0100

    add hyperparameter specification in train

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 import torch.nn as nn
 import numpy as np
@@ -7,6 +9,9 @@ from Discriminator import Discriminator
 from SRCNN import SRCNN
 from torchvision.models import vgg19
 
+m_seed = 1  # or use random.randint(1, 10000) for random reed
+random.seed(m_seed)
+torch.manual_seed(m_seed)
 
 
 def try_gpu():
@@ -21,11 +26,22 @@ def try_gpu():
     return device
 
 
-def train(model, img):
-    print(img)
-    y = model(img)
-    print(y)
-    pass
+def train(gen, disc, vgg):
+    # Specify hyperparameters Generator (SRCNN)
+    Epochs_generator = 100
+    iterations = 1000
+    mini_batch_size = 16  # Size 192x192
+    lr_generator = 1e-4
+    optimizer_gen = torch.optim.Adam(gen.parameters(), lr_generator)
+
+    # Specify hyperparameters Discriminator
+    alpha = 1e-5
+    beta = 5e-3
+    lr_disc = 1e-4
+    optimizer_disc = torch.optim.Adam(disc.parameters(), lr_disc)
+    epoch_both = 150
+
+    print("oka")
 
 
 if __name__ == '__main__':
@@ -42,3 +58,14 @@ if __name__ == '__main__':
     # VGG uses Coloured images originally so need to duplicate channels or something?
     vgg_original = vgg19(pretrained=True)
     vgg_cut = vgg_original.features
+    vgg_cut.to(device)
+
+    train(model, disc, vgg_cut)
+
+    # PSNR metric (equation 3)
+
+    # Loss functions:
+    l1_loss = nn.L1Loss()
+    adv_loss = ...
+
+    Total_D_loss = nn.BCELoss()  # Still needs to be averaged over number of samples
